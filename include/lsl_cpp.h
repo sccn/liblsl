966d0b36702594bc147d3aaa9aeb4bcddc2b07ea
Tristan Stenner <ttstenner@gmail.com>
2018-10-09 09:53:13 +0200

    More helpful error message with mismatching channel counts

--- include/lsl_cpp.h
+++ include/lsl_cpp.h
@@ -646,8 +646,10 @@ namespace lsl {
 
         /// Check whether a given data length matches the number of channels; throw if not
         void check_numchan(std::size_t N) const {
-            if (N != channel_count)
-                throw std::runtime_error("Provided element count does not match the stream's channel count.");
+			if (N != channel_count)
+				throw std::runtime_error("Provided element count (" + std::to_string(N) +
+			                             ") does not match the stream's channel count (" +
+			                             std::to_string(channel_count) + '.');
         }
 
         int32_t channel_count;
