c984ca232a472045140cb0a6298cc586a1361316
Tristan Stenner <ttstenner@gmail.com>
2019-05-10 10:24:20 +0200

    Reuse double conversion functions for floats

--- src/cast.cpp
+++ src/cast.cpp
@@ -2,14 +2,6 @@
 #include <locale>
 #include <sstream>
 
-template <> std::string lsl::to_string(float src) {
-	std::ostringstream os;
-	os.imbue(std::locale::classic());
-	os.precision(12);
-	os << src;
-	return os.str();
-}
-
 template <> std::string lsl::to_string(double src) {
 	std::ostringstream os;
 	os.imbue(std::locale::classic());
@@ -18,12 +10,8 @@ template <> std::string lsl::to_string(double src) {
 	return os.str();
 }
 
-template <> float lsl::from_string(const std::string &str) {
-	std::istringstream is(str);
-	is.imbue(std::locale::classic());
-	float res;
-	is >> res;
-	return res;
+template <> std::string lsl::to_string(float src) {
+	return lsl::to_string(static_cast<double>(src));
 }
 
 template <> double lsl::from_string(const std::string &str) {
@@ -32,4 +20,8 @@ template <> double lsl::from_string(const std::string &str) {
 	double res;
 	is >> res;
 	return res;
+}
+
+template <> float lsl::from_string(const std::string &src) {
+	return static_cast<float>(lsl::from_string<double>(src));
 }
