0643c86478efa64839cb61e7968bd27eaa3a9ceb
Tristan Stenner <ttstenner@gmail.com>
2019-05-10 10:24:20 +0200

    Reuse double conversion functions for floats

--- src/cast.h
+++ src/cast.h
@@ -8,13 +8,13 @@ template <typename T> inline std::string to_string(T src) {
 	return lslboost::lexical_cast<std::string>(src);
 }
 
-template <> std::string to_string(float src);
 template <> std::string to_string(double src);
+template <> std::string to_string(float src);
 
 template <typename T> inline T from_string(const std::string &str) {
 	return lslboost::lexical_cast<T>(str);
 }
 
-template <> float from_string(const std::string &str);
 template <> double from_string(const std::string &str);
+template <> float from_string(const std::string &str);
 } // namespace lsl
