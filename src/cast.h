dcc8c1dfbaaa709564cb9f2b02fe862e420fc660
Tristan Stenner <ttstenner@gmail.com>
2019-05-14 11:27:44 +0200

    Use same parameter name for string conversion functions

--- src/cast.h
+++ src/cast.h
@@ -4,12 +4,12 @@
 #include <string>
 
 namespace lsl {
-template <typename T> inline std::string to_string(T src) {
-	return lslboost::lexical_cast<std::string>(src);
+template <typename T> inline std::string to_string(T str) {
+	return lslboost::lexical_cast<std::string>(str);
 }
 
-template <> std::string to_string(double src);
-template <> std::string to_string(float src);
+template <> std::string to_string(double str);
+template <> std::string to_string(float str);
 
 template <typename T> inline T from_string(const std::string &str) {
 	return lslboost::lexical_cast<T>(str);
