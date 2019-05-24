81ae68d0cbd1307c88d1830ca3b561580c964e4d
Tristan Stenner <ttstenner@gmail.com>
2019-05-24 10:37:55 +0200

    Remove #defines already set by CMake

--- src/common.h
+++ src/common.h
@@ -116,26 +116,4 @@ namespace lsl {
 		~timeout_error();
 	};
 }
-
-
-// === Auto-generated target platform version defines (for Microsoft and asio) ===
-
-#ifndef WINVER
-#define WINVER 0x0501
 #endif
-
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0501
-#endif
-
-#ifndef _WIN32_WINDOWS
-#define _WIN32_WINDOWS 0x0501
-#endif
-
-#ifndef _WIN32_IE
-#define _WIN32_IE 0x0500
-#endif
-
-
-#endif
-
