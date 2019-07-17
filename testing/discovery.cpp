aa338d2a380775ec8f02f66a75d2ffbc87b05b6e
Tristan Stenner <ttstenner@gmail.com>
2019-07-17 13:36:52 +0200

    Also test the continuous resolver

--- testing/discovery.cpp
+++ testing/discovery.cpp
@@ -4,12 +4,14 @@
 namespace {
 
 TEST(Resolve, multi) {
+	lsl::continuous_resolver resolver("type", "Resolve");
 	std::vector<lsl::stream_outlet> outlets;
 	const int n = 3;
 	for (int i = 0; i < n; i++)
 		outlets.emplace_back(lsl::stream_info("resolvetest_" + std::to_string(i), "Resolve"));
-	auto found_stream_info = lsl::resolve_stream("type", "Resolve", 5, 2.0);
+	auto found_stream_info = lsl::resolve_stream("type", "Resolve", n, 2.0);
 	ASSERT_EQ(found_stream_info.size(), n);
+	ASSERT_EQ(resolver.results().size(), n);
 }
 
 TEST(Resolve, from_streaminfo) {
