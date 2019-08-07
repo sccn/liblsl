63cc4b43a099c94ce561f9a624dfdca5026ef522
Tristan Stenner <ttstenner@gmail.com>
2019-08-07 11:43:27 +0200

    Fix typo in timesync test, expand output

--- testing/timesync.cpp
+++ testing/timesync.cpp
@@ -5,15 +5,18 @@ namespace {
 
 TEST(timesync, simple) {
 	lsl::stream_outlet outlet(lsl::stream_info("timesync", "Test"));
-	auto si = lsl::resolve_stream("type", "Test", 5, 2.0);
+	auto si = lsl::resolve_stream("name", "timesync", 1, 2.0);
 	ASSERT_EQ(si.size(), 1);
 	lsl::stream_inlet inlet(si[0]);
+	double offset = inlet.time_correction(5.);
+	ASSERT_LE(offset, 0.001);
+	std::cout << "\tOffset (ms): " << (offset*1000) << '\n';
+
 	double remote_time, uncertainty;
-	double offset = inlet.time_correction(&remote_time, &uncertainty, 5.);
-
-	ASSERT_LE(offset, 0.01);
+	offset = inlet.time_correction(&remote_time, &uncertainty, 5.);
+	ASSERT_LE(offset, 0.001);
 	ASSERT_LE(remote_time, lsl::local_clock());
+	std::cout << "\tOffset (ms): " << (offset * 1000) << "±" << (uncertainty * 1000) << '\n';
 }
-
 
 } // namespace
