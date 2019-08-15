68d25fbafa358b2f5892c14b1389dcb10fa0ef5e
Tristan Stenner <ttstenner@gmail.com>
2019-08-15 12:42:11 +0200

    Linux build script: require only sh, not bash

--- standalone_compilation_linux.sh
+++ standalone_compilation_linux.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # This script builds the liblsl.so for linux machines without bigger quirks
 # and no recent CMake version, i.e. ARM boards and PCs with old distributions.
@@ -6,16 +6,17 @@
 # (pip install cmake) or as binary download from cmake.org
 
 set -x
-LSLGITREVISION="`git describe --tags HEAD`"
+# Try to read LSLGITREVISION from git if the variable isn't set
+echo ${LSLGITREVISION:="$(git describe --tags HEAD)"}
 ${CXX:-g++} -fPIC -fvisibility=hidden -O2 ${CFLAGS} -Ilslboost \
-	-DBOOST_ALL_NO_LIB -DBOOST_ASIO_SEPARATE_COMPILATION -D BOOST_THREAD_BUILD_DLL \
+	-DBOOST_ALL_NO_LIB \
 	-DLSL_LIBRARY_INFO_STR=\"${LSLGITREVISION:-"built from standalone build script"}\" \
 	src/*.cpp src/pugixml/pugixml.cpp \
-	lslboost/asio_objects.cpp \
 	lslboost/libs/atomic/src/lockpool.cpp \
 	lslboost/libs/chrono/src/chrono.cpp \
 	lslboost/libs/serialization/src/*.cpp \
-	lslboost/libs/thread/src/pthread/{once,thread}.cpp \
-	-shared -o liblsl.so -lpthread
+	lslboost/libs/thread/src/pthread/once.cpp \
+	lslboost/libs/thread/src/pthread/thread.cpp \
+	-shared -o liblsl.so -lpthread -lrt
 ${CC:-gcc} -O2 ${FLAGS} -Iinclude testing/lslver.c -o lslver -L. -llsl
 LD_LIBRARY_PATH=. ./lslver
