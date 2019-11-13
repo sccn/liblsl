8ce7f91c296c80cd62492063969cc97adabba731
Tristan Stenner <ttstenner@gmail.com>
2019-11-13 14:57:50 +0100

    Docs: sphinx < 2.0 compatibility

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'alabaster'
 
+master_doc = 'index' # for Sphinx < 2.0
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
