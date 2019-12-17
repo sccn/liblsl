cdce3aaf2044fa823beb7cac0e3ec376f1c7d78c
Tristan Stenner <ttstenner@gmail.com>
2019-12-17 13:13:45 +0100

    Avoid infinite loop if no socket can be bound

--- src/socket_utils.h
+++ src/socket_utils.h
@@ -20,15 +20,19 @@ namespace lsl {
 			} catch (lslboost::system::system_error &) { /* port occupied */ }
 		}
 		if (api_config::get_instance()->allow_random_ports()) {
-			while (true) {
+			for (int k=0; k < 100; ++k) {
 				uint16_t port = 1025 + rand()%64000;
 				try {
 					sock.bind(typename Protocol::endpoint(protocol,port));
 					return port;
 				} catch (lslboost::system::system_error &) { /* port occupied */ }
 			}
-		} else
-			throw std::runtime_error("All local ports were found occupied. You may have more open outlets on this machine than your PortRange setting allows (see Network Connectivity in the LSL wiki) or you have a problem with your network configuration.");
+		}
+		throw std::runtime_error(
+			"All local ports were found occupied. You may have more open outlets on this machine "
+			"than your PortRange setting allows (see "
+			"https://labstreaminglayer.readthedocs.io/info/network-connectivity.html) or you "
+			"have a problem with your network configuration.");
 	}
 
 	/// Bind to and listen at a socket (or acceptor) on a free port in the configured port range or throw an error otherwise.
@@ -37,12 +41,12 @@ namespace lsl {
 		for (int k=0,e=api_config::get_instance()->port_range(); k<e; k++) {
 			try {
 				sock.bind(typename Protocol::endpoint(protocol,(uint16_t)(k + api_config::get_instance()->base_port())));
-                sock.listen(backlog);
+				sock.listen(backlog);
 				return k + api_config::get_instance()->base_port();
 			} catch (lslboost::system::system_error &) { /* port occupied */ }
 		}
 		if (api_config::get_instance()->allow_random_ports()) {
-			while (true) {
+			for (int k = 0; k < 100; ++k) {
 				uint16_t port = 1025 + rand()%64000;
 				try {
 					sock.bind(typename Protocol::endpoint(protocol,port));
@@ -50,8 +54,12 @@ namespace lsl {
 					return port;
 				} catch (lslboost::system::system_error &) { /* port occupied */ }
 			}
-		} else
-			throw std::runtime_error("All local ports were found occupied. You may have more open outlets on this machine than your PortRange setting allows (see Network Connectivity in the LSL wiki) or you have a problem with your network configuration.");
+		}
+		throw std::runtime_error(
+			"All local ports were found occupied. You may have more open outlets on this machine "
+			"than your PortRange setting allows (see "
+			"https://labstreaminglayer.readthedocs.io/info/network-connectivity.html) or you "
+			"have a problem with your network configuration.");
 	}
 
 	/// Measure the endian conversion performance of this machine.
