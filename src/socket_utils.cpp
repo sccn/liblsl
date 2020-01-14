8fc29bf4e2a13431f3a8193ba3cebaae0cf544ca
Tristan Stenner <ttstenner@gmail.com>
2020-01-14 18:31:40 +0100

    Use error codes in port binding routine ("Break on exception" finally becomes usable), move implementation to cpp file, reduce code duplication

--- src/socket_utils.cpp
+++ src/socket_utils.cpp
@@ -1,7 +1,7 @@
 #include "socket_utils.h"
-#include <boost/endian/conversion.hpp>
 #include "common.h"
-
+#include <boost/endian/conversion.hpp>
+#include <boost/system/system_error.hpp>
 
 // === Implementation of the socket utils ===
 
@@ -16,4 +16,43 @@ double lsl::measure_endian_performance() {
 	for (k=0; ((int)k & 0xFF) != 0 || lsl_clock()<t_end; k++)
 		lslboost::endian::endian_reverse_inplace(data);
 	return k;
+}
+
+template <typename Socket, typename Protocol>
+uint16_t bind_port_in_range_(Socket &sock, Protocol protocol) {
+	const api_config *cfg = api_config::get_instance();
+	lslboost::system::error_code ec;
+	for (uint16_t port = cfg->base_port(), e = port + cfg->port_range(); port < e; port++) {
+		sock.bind(typename Protocol::endpoint(protocol, port), ec);
+		if (ec == lslboost::system::errc::address_in_use) continue;
+		if (!ec) return port;
+	}
+	if (cfg->allow_random_ports()) {
+		for (int k = 0; k < 100; ++k) {
+			uint16_t port = 1025 + rand() % 64000;
+			sock.bind(typename Protocol::endpoint(protocol, port), ec);
+			if (ec == lslboost::system::errc::address_in_use) continue;
+			if (!ec) return port;
+		}
+	}
+	return 0;
+}
+
+const std::string all_ports_bound_msg(
+	"All local ports were found occupied. You may have more open outlets on this machine than your "
+	"PortRange setting allows (see "
+	"https://labstreaminglayer.readthedocs.io/info/network-connectivity.html"
+	") or you have a problem with your network configuration.");
+
+uint16_t lsl::bind_port_in_range(udp::socket &acc, udp protocol) {
+	uint16_t port = bind_port_in_range_(acc, protocol);
+	if (!port) throw std::runtime_error(all_ports_bound_msg);
+	return port;
+}
+
+uint16_t lsl::bind_and_listen_to_port_in_range(tcp::acceptor &sock, tcp protocol, int backlog) {
+	uint16_t port = bind_port_in_range_(sock, protocol);
+	if (!port) throw std::runtime_error(all_ports_bound_msg);
+	sock.listen(backlog);
+	return port;
 }
--- src/socket_utils.h
+++ src/socket_utils.h
@@ -2,69 +2,25 @@
 #define SOCKET_UTILS_H
 
 #include "api_config.h"
-#include <boost/system/system_error.hpp>
 #include <boost/asio/detail/chrono.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ip/udp.hpp>
+
+using namespace lslboost::asio::ip;
 
 namespace lsl {
-	inline lslboost::asio::chrono::milliseconds timeout_sec(double timeout_seconds) {
-		return lslboost::asio::chrono::milliseconds(static_cast<unsigned int>(1000*timeout_seconds));
-	}
-
-    /// Bind a socket (or acceptor) to a free port in the configured port range or throw an error otherwise.
-	template <class Socket, class Protocol>
-	uint16_t bind_port_in_range(Socket &sock, Protocol protocol) {
-		for (int k=0,e=api_config::get_instance()->port_range(); k<e; k++) {
-			try {
-				sock.bind(typename Protocol::endpoint(protocol,(uint16_t)(k + api_config::get_instance()->base_port())));
-				return k + api_config::get_instance()->base_port();
-			} catch (lslboost::system::system_error &) { /* port occupied */ }
-		}
-		if (api_config::get_instance()->allow_random_ports()) {
-			for (int k=0; k < 100; ++k) {
-				uint16_t port = 1025 + rand()%64000;
-				try {
-					sock.bind(typename Protocol::endpoint(protocol,port));
-					return port;
-				} catch (lslboost::system::system_error &) { /* port occupied */ }
-			}
-		}
-		throw std::runtime_error(
-			"All local ports were found occupied. You may have more open outlets on this machine "
-			"than your PortRange setting allows (see "
-			"https://labstreaminglayer.readthedocs.io/info/network-connectivity.html) or you "
-			"have a problem with your network configuration.");
-	}
-
-	/// Bind to and listen at a socket (or acceptor) on a free port in the configured port range or throw an error otherwise.
-	template <class Socket, class Protocol>
-	uint16_t bind_and_listen_to_port_in_range(Socket &sock, Protocol protocol, int backlog) {
-		for (int k=0,e=api_config::get_instance()->port_range(); k<e; k++) {
-			try {
-				sock.bind(typename Protocol::endpoint(protocol,(uint16_t)(k + api_config::get_instance()->base_port())));
-				sock.listen(backlog);
-				return k + api_config::get_instance()->base_port();
-			} catch (lslboost::system::system_error &) { /* port occupied */ }
-		}
-		if (api_config::get_instance()->allow_random_ports()) {
-			for (int k = 0; k < 100; ++k) {
-				uint16_t port = 1025 + rand()%64000;
-				try {
-					sock.bind(typename Protocol::endpoint(protocol,port));
-					sock.listen(backlog);
-					return port;
-				} catch (lslboost::system::system_error &) { /* port occupied */ }
-			}
-		}
-		throw std::runtime_error(
-			"All local ports were found occupied. You may have more open outlets on this machine "
-			"than your PortRange setting allows (see "
-			"https://labstreaminglayer.readthedocs.io/info/network-connectivity.html) or you "
-			"have a problem with your network configuration.");
-	}
-
-	/// Measure the endian conversion performance of this machine.
-	double measure_endian_performance();
+inline lslboost::asio::chrono::milliseconds timeout_sec(double timeout_seconds) {
+	return lslboost::asio::chrono::milliseconds(static_cast<unsigned int>(1000 * timeout_seconds));
 }
 
-#endif
+/// Bind a socket to a free port in the configured port range or throw an error otherwise.
+uint16_t bind_port_in_range(udp::socket &sock, udp protocol);
 
+/// Bind and listen to an acceptor on a free port in the configured port range or throw an error.
+uint16_t bind_and_listen_to_port_in_range(tcp::acceptor &acc, tcp protocol, int backlog);
+
+/// Measure the endian conversion performance of this machine.
+double measure_endian_performance();
+} // namespace lsl
+
+#endif
