e4e3a4f6c12d511d150f7e417119f700b99c95ad
Tristan Stenner <ttstenner@gmail.com>
2020-02-19 12:09:50 +0100

    CMake: allow find_package in case liblsl is already loaded

--- cmake/LSLConfig.cmake
+++ cmake/LSLConfig.cmake
@@ -1,2 +1,8 @@
-include("${CMAKE_CURRENT_LIST_DIR}/LSLTargets.cmake")
+if(TARGET lsl AND NOT TARGET LSL::lsl)
+	add_library(LSL::lsl ALIAS lsl)
+endif()
+
+if(NOT TARGET LSL::lsl)
+	include("${CMAKE_CURRENT_LIST_DIR}/LSLTargets.cmake")
+endif()
 include("${CMAKE_CURRENT_LIST_DIR}/LSLCMake.cmake")
