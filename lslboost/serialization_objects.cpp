6df5a1569a4a04326a8118e2060593854c7ff4c2
Tristan Stenner <ttstenner@gmail.com>
2020-04-03 12:12:53 +0200

    Compile Boost.Serialization objects for Windows in serialization object file

--- lslboost/serialization_objects.cpp
+++ lslboost/serialization_objects.cpp
@@ -8,3 +8,6 @@
 #include "libs/serialization/src/extended_type_info.cpp"
 #include "libs/serialization/src/extended_type_info_typeid.cpp"
 #include "libs/serialization/src/void_cast.cpp"
+#ifdef _WIN32
+#include "lslboost/libs/serialization/src/codecvt_null.cpp"
+#endif
