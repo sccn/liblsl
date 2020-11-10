798f8ffab89c146db67f6751864babc3dd263057
Tristan Stenner <ttstenner@gmail.com>
2020-11-10 11:21:09 +0100

    Remove unused implementation files from build scripts

--- standalone_compilation_linux.sh
+++ standalone_compilation_linux.sh
@@ -8,17 +8,14 @@
 set -e -x
 # Try to read LSLGITREVISION from git if the variable isn't set
 echo ${LSLGITREVISION:="$(git describe --tags HEAD)"}
-${CXX:-g++} -fPIC -fvisibility=hidden -O2 ${CFLAGS} -Ilslboost \
+${CXX:-g++} -fPIC -fvisibility=hidden -O2 ${CFLAGS} ${CXXFLAGS} -Ilslboost \
 	-DBOOST_ALL_NO_LIB \
 	-DBOOST_THREAD_DONT_PROVIDE_INTERRUPTIONS \
 	-DBOOST_ASIO_STANDALONE \
 	-DLOGURU_DEBUG_LOGGING=0 \
 	-DLSL_LIBRARY_INFO_STR=\"${LSLGITREVISION:-"built from standalone build script"}\" \
 	src/*.cpp src/pugixml/pugixml.cpp src/loguru/loguru.cpp \
-	lslboost/libs/chrono/src/chrono.cpp \
 	lslboost/serialization_objects.cpp \
-	lslboost/libs/thread/src/pthread/once.cpp \
-	lslboost/libs/thread/src/pthread/thread.cpp \
 	${LDFLAGS} \
 	-shared -o liblsl.so -lpthread -lrt -ldl
 ${CC:-gcc} -O2 ${CFLAGS} -Iinclude testing/lslver.c -o lslver -L. -llsl
