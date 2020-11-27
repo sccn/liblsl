371661bad7e4391336dcb2b92b50094e30c8d20f
Tristan Stenner <ttstenner@gmail.com>
2020-11-27 12:21:46 +0100

    Replace last instance of Boost lexical_cast with std::istringstream

--- src/cast.cpp
+++ src/cast.cpp
@@ -1,5 +1,4 @@
 #include "cast.h"
-#include <boost/lexical_cast.hpp>
 #include <iomanip>
 #include <locale>
 #include <sstream>
@@ -21,23 +20,18 @@ template <> std::string to_string(float str) {
 }
 
 template <typename T> T from_string(const std::string &str) {
-	return lslboost::lexical_cast<T>(str);
-}
-
-template <> double from_string(const std::string &str) {
 	std::istringstream is(str);
 	is.imbue(std::locale::classic());
-	double res;
+	T res;
 	is >> res;
 	return res;
 }
 
-template <> float from_string(const std::string &str) {
-	return static_cast<float>(from_string<double>(str));
-}
-
-// Explicit template instantiations so lexical_cast doesn't have to be included
+// Explicit template instantiations so from_string doesn't have to be compiled
 // in every compilation unit
+template float from_string(const std::string &);
+template double from_string(const std::string &);
+
 template signed char from_string(const std::string &);
 template char from_string(const std::string &);
 template int16_t from_string(const std::string &);
