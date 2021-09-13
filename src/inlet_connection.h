b0f2f64e183e444ee236ce2f765ca45d7eeab08a
Tristan Stenner <ttstenner@gmail.com>
2021-09-13 19:31:49 +0200

    Fall back to a plain mutex for C++11 builds
    This removes the dependency on Boost.Thread shared_mutex and is about as fast
    when no stream recovery is in progress.

--- src/inlet_connection.h
+++ src/inlet_connection.h
@@ -13,21 +13,18 @@
 #include <string>
 #include <thread>
 
-/* shared_mutex was added in C++17 so we use the boost shared_mutex when
+/* shared_mutex was added in C++17 so we fall back to a plain mutex when
 building for C++11 / C++14 or MSVC <= 2019 */
 #if __cplusplus >= 201703L || _MSC_VER >= 1925
-#include <mutex>
 #include <shared_mutex>
 using shared_mutex_t = std::shared_mutex;
 using shared_lock_t = std::shared_lock<std::shared_mutex>;
-using unique_lock_t = std::unique_lock<std::shared_mutex>;
 #else
-#include <boost/thread/lock_types.hpp>
-#include <boost/thread/shared_mutex.hpp>
-using shared_mutex_t = lslboost::shared_mutex;
-using shared_lock_t = lslboost::shared_lock<lslboost::shared_mutex>;
-using unique_lock_t = lslboost::unique_lock<lslboost::shared_mutex>;
+using shared_mutex_t = std::mutex;
+using shared_lock_t = std::unique_lock<std::mutex>;
 #endif
+
+using unique_lock_t = std::unique_lock<shared_mutex_t>;
 
 namespace asio = lslboost::asio;
 using asio::ip::tcp;
