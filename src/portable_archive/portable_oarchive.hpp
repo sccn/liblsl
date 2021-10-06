b7a11bce1d464ce575cdb0ab28c9323a6450a1b9
Tristan Stenner <ttstenner@gmail.com>
2021-10-06 18:36:55 +0200

    Add floating point constants, assert equality with Boost's fpclassify

--- src/portable_archive/portable_oarchive.hpp
+++ src/portable_archive/portable_oarchive.hpp
@@ -10,6 +10,28 @@
 #include <boost/archive/basic_binary_oarchive.hpp>
 #endif
 
+#include <cstring>
+
+template <typename T> struct FPTraits {};
+template <> struct FPTraits<double> {
+	using bits = uint64_t;
+	static constexpr bits sign = (0x80000000ull) << 32, exponent = (0x7ff00000ll) << 32,
+						  significand = ((0x000fffffll) << 32) + (0xfffffffful);
+	bits get_bits(double f) {
+		bits b;
+		std::memcpy(&b, &f, sizeof(bits));
+		return b;
+	}
+};
+template <> struct FPTraits<float> {
+	using bits = uint32_t;
+	static constexpr bits sign = 0x80000000u, exponent = 0x7f800000, significand = 0x007fffff;
+	bits get_bits(float f) {
+		bits b;
+		std::memcpy(&b, &f, sizeof(bits));
+		return b;
+	}
+};
 
 namespace eos {
 
@@ -195,6 +217,9 @@ namespace eos {
 		save(const T & t, dummy<3> = 0)
 		{
 			using traits = typename fp::detail::fp_traits<T>::type;
+			using newtraits = FPTraits<T>;
+			static_assert(std::is_same<typename traits::bits, typename newtraits::bits>::value,
+				"Wrong corresponding int type");
 
 			// if the no_infnan flag is set we must throw here
 			if (get_flags() & no_infnan && !fp::isfinite(t))
@@ -207,9 +232,13 @@ namespace eos {
 			// after reading the note above you still might decide to 
 			// deactivate this static assert and try if it works out.
 			typename traits::bits bits;
-			BOOST_STATIC_ASSERT(sizeof(bits) == sizeof(T));
-			BOOST_STATIC_ASSERT(std::numeric_limits<T>::is_iec559);
-            
+			static_assert(sizeof(bits) == sizeof(T), "mismatching type sizes");
+			static_assert(std::numeric_limits<T>::is_iec559, "float representation differs from IEC559");
+
+			static_assert(traits::exponent == newtraits::exponent, "mismatching exponent");
+			static_assert(traits::significand == newtraits::significand, "mismatching significand");
+			static_assert(traits::sign == newtraits::sign, "mismatching sign bit");
+
 			switch (fp::fpclassify(t)) {
 			case FP_NAN: bits = traits::exponent | traits::significand; break;
 			case FP_INFINITE: bits = traits::exponent | (t<0) * traits::sign; break;
