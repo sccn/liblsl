e832358da329e26a61c26ad604a65c51c5849f54
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-07-07 17:32:47 -0400

    Keep original order of cmake invocations as much as possible

--- cmake/CompilerSettings.cmake
+++ cmake/CompilerSettings.cmake
@@ -5,17 +5,20 @@ set(CMAKE_VISIBILITY_INLINES_HIDDEN ON)
 # generate a compilation database (compile_commands.json) for clang tooling
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-# Enable position independent code for shared libraries
-if(NOT LSL_BUILD_STATIC OR UNIX)
-    # shared libraries require relocatable symbols so we enable them by default
-    set(CMAKE_POSITION_INDEPENDENT_CODE ON)
-endif()
+set(LSL_WINVER "0x0601" CACHE STRING
+        "Windows version (_WIN32_WINNT) to target (defaults to 0x0601 for Windows 7)")
 
 # Determine library type
 if(LSL_BUILD_STATIC)
     set(LSL_LIB_TYPE STATIC)
 else()
     set(LSL_LIB_TYPE SHARED)
+endif()
+
+# Enable position independent code for shared libraries
+if(NOT LSL_BUILD_STATIC OR UNIX)
+    # shared libraries require relocatable symbols so we enable them by default
+    set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 endif()
 
 # Enable optimizations if requested
@@ -27,9 +30,6 @@ endif()
 # Platform-specific settings
 if(WIN32)
     add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-    
-    set(LSL_WINVER "0x0601" CACHE STRING
-            "Windows version (_WIN32_WINNT) to target (defaults to 0x0601 for Windows 7)")
 elseif(APPLE)
     set(CMAKE_MACOSX_RPATH ON)
 endif()
