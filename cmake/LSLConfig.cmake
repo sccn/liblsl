ef64c82395fc5358c307d07a543d785baac41e1f
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-07-08 01:57:00 -0400

    Cleanup CMake redundancies and use FILE_SET for headers -- only solution that installed non-flat header tree by transitive dependencies.

--- cmake/LSLConfig.cmake
+++
@@ -1,8 +0,0 @@
-if(TARGET lsl AND NOT TARGET LSL::lsl)
-	add_library(LSL::lsl ALIAS lsl)
-endif()
-
-if(NOT TARGET LSL::lsl)
-	include("${CMAKE_CURRENT_LIST_DIR}/LSLTargets.cmake")
-endif()
-include("${CMAKE_CURRENT_LIST_DIR}/LSLCMake.cmake")
