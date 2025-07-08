a51c463a1b231dd67e9183c4049788f898f391d1
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-07-07 21:31:01 -0400

    Reorder lsl cmake commands for slightly more logical grouping.

--- cmake/TargetLib.cmake
+++ cmake/TargetLib.cmake
@@ -9,29 +9,7 @@ add_library(lsl ${LSL_LIB_TYPE} src/buildinfo.cpp)
 
 # Configure main library
 
-# Set compile definitions for lsl
-target_compile_definitions(lsl
-    PUBLIC
-        # defines for LSL_CPP_API export header (shared: dllimport/dllexport)
-        $<IF:$<BOOL:${LSL_BUILD_STATIC}>,LIBLSL_STATIC,LIBLSL_EXPORTS>
-        # don't use #pragma(lib) in MSVC builds. TODO: Maybe this can be inherited from lslobj or removed on lslobj?
-        $<$<CXX_COMPILER_ID:MSVC>:LSLNOAUTOLINK>
-)
-
-target_link_libraries(lsl
-        PRIVATE
-        lslobj  # TODO: If this is public, does that improve inheritance of includes and compile definitions?
-        lslboost  # TODO: Shouldn't be needed -- lslobj already links it
-        ${PUGIXML_LIBRARIES}
-)
-
-# Includes. TODO: Can we not inherit these from lslobj?
-target_include_directories(lsl
-    INTERFACE
-        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
-        $<INSTALL_INTERFACE:include>
-)
-
+# Some naming metadata for export
 set_target_properties(lsl PROPERTIES
         VERSION ${liblsl_VERSION_MAJOR}.${liblsl_VERSION_MINOR}.${liblsl_VERSION_PATCH}
         SOVERSION ${LSL_ABI_VERSION}
@@ -44,3 +22,27 @@ if(LSL_FORCE_FANCY_LIBNAME)
             DEBUG_POSTFIX "-debug"
     )
 endif()
+
+# Includes. TODO: Can we not inherit these from lslobj?
+target_include_directories(lsl
+        INTERFACE
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+        $<INSTALL_INTERFACE:include>
+)
+
+# Link dependencies. The biggest dependency is lslobj, which contains the bulk of the library code.
+target_link_libraries(lsl
+    PRIVATE
+        lslobj  # TODO: If this is public, does that improve inheritance of includes and compile definitions?
+        lslboost  # TODO: Shouldn't be needed -- lslobj already links it
+        ${PUGIXML_LIBRARIES}
+)
+
+# Set compile definitions for lsl
+target_compile_definitions(lsl
+    PUBLIC
+        # defines for LSL_CPP_API export header (shared: dllimport/dllexport)
+        $<IF:$<BOOL:${LSL_BUILD_STATIC}>,LIBLSL_STATIC,LIBLSL_EXPORTS>
+        # don't use #pragma(lib) in MSVC builds. TODO: Maybe this can be inherited from lslobj or removed on lslobj?
+        $<$<CXX_COMPILER_ID:MSVC>:LSLNOAUTOLINK>
+)
