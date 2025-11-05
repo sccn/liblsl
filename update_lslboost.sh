d23ac0623096883267b48526d64d38bd44566149
zeyus <dev@zeyus.com>
2025-11-05 14:09:52 +0100

    Update boost (with update script) to version v1.89.0

--- update_lslboost.sh
+++ update_lslboost.sh
@@ -1,7 +1,7 @@
 # the absolute path to the extracted boost source archive (https://www.boost.org/users/download/)
 set -e
 set -x
-BOOSTPATH=/tmp/boost_1_84_0
+BOOSTPATH=/tmp/boost_1_89_0
 TMPPATH=/tmp/lslboost
 
 # copy all needed boost files and rename all mentions of boost to lslboost
@@ -12,4 +12,3 @@ rm -f $TMPPATH/Jamroot
 find $TMPPATH -type d -and \( -name build -o -name test -o -name edg -o -name dmc -o -name msvc?0 -o -name bcc* \) -print0 | xargs -0 rm -rf
 
 rsync -HAXavr --del $TMPPATH/boost lslboost
-
