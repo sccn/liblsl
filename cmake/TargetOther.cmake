177b84d0f33169b6d794f01077967bf67d73903b
Chadwick Boulay <chadwick.boulay@gmail.com>
2026-01-10 01:24:14 -0500

    Replace usage of deprecated LSLCMake.cmake with one-liners

--- cmake/TargetOther.cmake
+++ cmake/TargetOther.cmake
@@ -1,16 +1,17 @@
-include(cmake/LSLCMake.cmake)  # Needed for `installLSLApp` and `LSLGenerateCPackConfig`
+# Build utilities
+include(GNUInstallDirs)
 
-    # Build utilities
 add_executable(lslver testing/lslver.c)
 target_link_libraries(lslver PRIVATE lsl)
 if(NOT IOS)
-installLSLApp(lslver)
-endif(NOT IOS)
+    install(TARGETS lslver RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}")
+endif()
+
 if(NOT WIN32 AND LSL_TOOLS)
     add_executable(blackhole testing/blackhole.cpp)
     target_link_libraries(blackhole PRIVATE Threads::Threads)
     target_include_directories(blackhole PRIVATE "thirdparty/asio/")
     if(NOT IOS)
-        installLSLApp(blackhole)
-    endif(NOT IOS)
+        install(TARGETS blackhole RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}")
+    endif()
 endif()
