64f8f950efc8ca2fc800f7534c18ceae942270e9
Chadwick Boulay <chadwick.boulay@gmail.com>
2026-01-17 20:48:59 -0500

    Fetch pugixml v1.15 via FetchContent; hide symbols

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -3,17 +3,37 @@ find_package(Threads REQUIRED)
 # Git version information
 include(cmake/GitVersion.cmake)
 
-# PugiXML dependency
-if(LSL_BUNDLED_PUGIXML)
-    message(STATUS "Using bundled pugixml")
-    # We will later add the pugixml source files to the lslobj target
-else()
-    message(STATUS "Using system pugixml")
-    find_package(pugixml REQUIRED)
-    if(NOT TARGET pugixml::pugixml)
-        add_library(pugixml::pugixml ALIAS pugixml)
-    endif()
+# PugiXML dependency via FetchContent
+# Always use static pugixml to avoid symbol visibility issues
+# Note: We use features of pugixml >= 1.15. Once that is available
+#  on target platforms' package managers, then we can allow using
+#  system pugixml.
+include(FetchContent)
+set(PUGIXML_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(PUGIXML_NO_EXCEPTIONS OFF CACHE BOOL "" FORCE)
+set(PUGIXML_INSTALL OFF CACHE BOOL "" FORCE)
+# Force static library even if parent project sets BUILD_SHARED_LIBS
+set(PUGIXML_BUILD_SHARED_AND_STATIC_LIBS OFF CACHE BOOL "" FORCE)
+set(_lsl_saved_build_shared_libs ${BUILD_SHARED_LIBS})
+set(BUILD_SHARED_LIBS OFF)
+FetchContent_Declare(
+    pugixml
+    GIT_REPOSITORY https://github.com/zeux/pugixml.git
+    GIT_TAG v1.15
+    GIT_SHALLOW TRUE
+    EXCLUDE_FROM_ALL
+)
+FetchContent_MakeAvailable(pugixml)
+set(BUILD_SHARED_LIBS ${_lsl_saved_build_shared_libs})
+unset(_lsl_saved_build_shared_libs)
+if(TARGET pugixml AND NOT TARGET pugixml::pugixml)
+    add_library(pugixml::pugixml ALIAS pugixml)
 endif()
+# Hide pugixml symbols - apply hidden visibility to the pugixml target
+set_target_properties(pugixml PROPERTIES
+    CXX_VISIBILITY_PRESET hidden
+    VISIBILITY_INLINES_HIDDEN ON
+)
 
 # Create lslboost target
 add_library(lslboost INTERFACE)
